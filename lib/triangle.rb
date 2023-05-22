d7280bde02ec4951088c1ca0cbb7191cd83f0005
brigitteorina <brigitteorina@gmail.com>
2023-05-22 20:35:25 +0300

    complete

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
 class Triangle
-  # write code here
+  attr_reader :side1, :side2, :side3
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+    validate_triangle
+  end
+
+  def kind
+    if side1 == side2 && side2 == side3
+      :equilateral
+    elsif side1 == side2 || side2 == side3 || side1 == side3
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  private
+
+  def validate_triangle
+    if side1 <= 0 || side2 <= 0 || side3 <= 0
+      raise TriangleError, "Sides must be greater than 0."
+    elsif side1 + side2 <= side3 || side2 + side3 <= side1 || side1 + side3 <= side2
+      raise TriangleError, "Invalid triangle lengths."
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
